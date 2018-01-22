082fba13c713661f34975741c3b52979d9fbce44
aya <aya@github.com>
2018-01-22 15:40:35 -0400

    finish

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.acme.csci3130_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String Message ="Hello World~";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void SendMessage (View view){
+        Intent to =new Intent(this, Next.class);
+        startActivity(to);
     }
 }
